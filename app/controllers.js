9702d3d84c10e6c289d205f54fecced5506b1c62
Tushar Gautam <tushar.rishav@gmail.com>
2015-09-18 18:26:33 +0530

    Pagination added

--- app/controllers.js
+++ app/controllers.js
@@ -1,42 +1,24 @@
 angular.module('app.controllers', [
-	'app.directives',
-	'ui.bootstrap'
+	'app.directives'
 ])
-	.controller('blogController', ['$scope', '$rootScope','$http', function($scope,$rootScope,$http){
+	.controller('blogController', ['$scope','$http', function($scope,$http){
 		$scope.currentPage = 1;
-		$scope.numPerPage = 4;
-		$scope.maxSize = 4;
-		$scope.filteredPosts=[];
-		
+		$scope.pageSize = 3;
 		$http.get('data/blog_detail.json').success(function(data){
 			$scope.posts = data;
-			$rootScope.postData = data;
+			$scope.numPages = function() {
+		        return Math.ceil($scope.posts.length / $scope.pageSize);
+		    };
 		});
-	  console.log($rootScope.postData);	// undefined. WHY?
-	  //console.log($scope.posts);
-	  $scope.setPage = function (pageNo) {
-			    $scope.currentPage = pageNo;
-	  };
-	  $scope.pageChanged = function() {
-			    console.log('Page changed to: ' + $scope.currentPage);
-	  };
-
-	  $scope.$watch('currentPage + numPerPage', function() {
-
-	    var begin = (($scope.currentPage - 1) * $scope.numPerPage);
-	    end = begin + $scope.numPerPage;
-
-	    // $scope.posts is not available here. WHY??
-	    
-	    // CHANGE THE BELOW CODE ONCE POST IS AVAILABLE IN THIS SCOPE	
-	    $scope.filteredPosts = [];//$rootScope.postData.slice(begin, end);
-	    console.log($scope.filteredPosts);
-	  });
-
-
 	}])
-	.controller('overviewController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
+	.controller('overviewController', ['$scope', '$http', function($scope, $http){
 		$http.get('data/blog_detail.json').success(function(data){
 			$scope.post = data[$routeParams.id];
 		});
-	}]);
+	}])
+	.filter('startFrom', function() {	// custom filter for pagination
+    return function(input, start) {
+        start = +start; //parse to int
+        return input.slice(start);
+    }
+	});
